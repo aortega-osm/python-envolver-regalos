60b9352f717258d53f9fa01da4c3b499de2059f6
alexo <aortega@osm-soft.io>
2023-05-02 15:41:33 -0300

    Se añadio el ejercicio realizado

---
+++ envolver-regalos.py
@@ -0,0 +1,8 @@
+regalo1=input("Regalo 1:")
+regalo2=input("Regalo 2:")
+regalo3=input("Regalo 3:")
+regalo4=input("Regalo 4:")
+
+listaRegalos=(regalo1,regalo2,regalo3,regalo4)
+for regalo in listaRegalos:
+    print(f"*****{regalo}*****")
